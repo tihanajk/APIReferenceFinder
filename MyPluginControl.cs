c3e15ad1d6ecce4e98c52153d04121aafb26d1e8
Tihana Jurković-Kormo <tjkormo@span.eu>
2024-07-18 12:05:54 +0200

    initial commit

---
+++ MyPluginControl.cs
@@ -0,0 +1,396 @@
+﻿using McTools.Xrm.Connection;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Services.Description;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using XrmToolBox.Extensibility;
+
+namespace MyTool
+{
+    public partial class MyPluginControl : PluginControlBase
+    {
+        private Settings mySettings;
+
+        public MyPluginControl()
+        {
+            InitializeComponent();
+            InitializeFlowView();
+            InitializeJSView();
+        }
+
+        private void MyPluginControl_Load(object sender, EventArgs e)
+        {
+            ShowInfoNotification("Hello there", new Uri("https://www.google.com/"));
+
+            // Loads or creates the settings for the plugin
+            if (!SettingsManager.Instance.TryLoad(GetType(), out mySettings))
+            {
+                mySettings = new Settings();
+
+                LogWarning("Settings not found => a new settings file has been created!");
+            }
+            else
+            {
+                LogInfo("Settings found and loaded");
+            }
+        }
+
+        private void tsbClose_Click(object sender, EventArgs e)
+        {
+            CloseTool();
+        }
+
+        private void tsbSample_Click(object sender, EventArgs e)
+        {
+            // The ExecuteMethod method handles connecting to an
+            // organization if XrmToolBox is not yet connected
+            ExecuteMethod(GetAccounts);
+        }
+
+        private void GetAccounts()
+        {
+            WorkAsync(new WorkAsyncInfo
+            {
+                Message = "Getting accounts",
+                Work = (worker, args) =>
+                {
+                    args.Result = Service.RetrieveMultiple(new QueryExpression("th_dev")
+                    {
+                        TopCount = 50
+                    });
+                },
+                PostWorkCallBack = (args) =>
+                {
+                    if (args.Error != null)
+                    {
+                        MessageBox.Show(args.Error.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    var result = args.Result as EntityCollection;
+                    if (result != null)
+                    {
+                        MessageBox.Show($"Found {result.Entities.Count} devs");
+                    }
+                }
+            });
+        }
+
+        /// <summary>
+        /// This event occurs when the plugin is closed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MyPluginControl_OnCloseTool(object sender, EventArgs e)
+        {
+            // Before leaving, save the settings
+            SettingsManager.Instance.Save(GetType(), mySettings);
+        }
+
+        /// <summary>
+        /// This event occurs when the connection has been updated in XrmToolBox
+        /// </summary>
+        public override void UpdateConnection(IOrganizationService newService, ConnectionDetail detail, string actionName, object parameter)
+        {
+            base.UpdateConnection(newService, detail, actionName, parameter);
+
+            if (mySettings != null && detail != null)
+            {
+                mySettings.LastUsedOrganizationWebappUrl = detail.WebApplicationUrl;
+                LogInfo("Connection has changed to: {0}", detail.WebApplicationUrl);
+            }
+        }
+
+        private class ListObject
+        {
+            public string name { get; set; }
+            public string value { get; set; }
+        }
+
+        private void GetAPIs()
+        {
+            WorkAsync(new WorkAsyncInfo()
+            {
+                Message = "Getting APIs",
+                AsyncArgument = null,
+                Work = (worker, args) =>
+                {
+                    var query = new QueryExpression("customapi");
+                    query.ColumnSet.AddColumns("customapiid", "displayname", "uniquename");
+                    query.AddOrder("displayname", OrderType.Ascending);
+                    args.Result = Service.RetrieveMultiple(query);
+                },
+                PostWorkCallBack = (args) =>
+                {
+                    if (args.Error != null)
+                    {
+                        MessageBox.Show(args.Error.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    var result = args.Result as EntityCollection;
+
+                    ComboboxAPIs.Items.Clear();
+
+                    if (result != null)
+                    {
+                        var items = new List<ListObject>();
+                        //items.Add(new ListObject()
+                        //{
+                        //    name = "",
+                        //    value = null
+                        //});
+
+                        foreach (var ent in result.Entities)
+                        {
+                            var listObject = new ListObject();
+                            listObject.name = $"{(string)ent["displayname"]} ({(string)ent["uniquename"]})";
+                            listObject.value = (string)ent["uniquename"];
+
+                            items.Add(listObject);
+                        }
+
+                        ComboboxAPIs.DataSource = items;
+                        ComboboxAPIs.DisplayMember = "name";
+                        ComboboxAPIs.ValueMember = "value";
+
+                    }
+                }
+            });
+        }
+
+        private void GetFlows()
+        {
+            var api = ComboboxAPIs.SelectedItem as ListObject;
+            if (api == null) return;
+
+            var apiLogName = api.value;
+            if (apiLogName == null) return;
+
+            WorkAsync(new WorkAsyncInfo()
+            {
+                Message = "Fetching flows",
+                AsyncArgument = null,
+                Work = (worker, args) =>
+                {
+                    var fetchXml = $@"<fetch>
+                              <entity name=""workflow"">
+                                <attribute name=""workflowid"" />
+                                <attribute name=""name"" />
+                                <filter type=""and"">
+                                  <condition attribute=""category"" operator=""eq"" value=""5"" />
+                                  <condition attribute=""clientdata"" operator=""like"" value=""%&quot;actionName&quot;:&quot;{apiLogName}&quot;,%"" />
+                                </filter>
+                                <order attribute=""name"" />
+                              </entity>
+                            </fetch>";
+                    args.Result = Service.RetrieveMultiple(new FetchExpression(fetchXml));
+                },
+                PostWorkCallBack = (args) =>
+                {
+                    if (args.Error != null)
+                    {
+                        MessageBox.Show(args.Error.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    var result = args.Result as EntityCollection;
+
+                    ListOfFlows.Items.Clear();
+
+                    if (result != null)
+                    {   
+                        foreach (var f in result.Entities)
+                        {
+                            var flowName = (string)f["name"];
+                            ListViewItem item = new ListViewItem(flowName);
+                            item.SubItems.Add("link");
+                            ListOfFlows.Items.Add(item);
+                        }
+                    }
+                }
+
+            });
+        }
+
+        private void GetJS()
+        {
+            var api = ComboboxAPIs.SelectedItem as ListObject;
+            if (api == null) return;
+
+            var apiLogName = api.value;
+            if (apiLogName == null) return;
+
+            WorkAsync(new WorkAsyncInfo()
+            {               
+                AsyncArgument = null,
+                Work = (worker, args) =>
+                {
+                    var query = new QueryExpression("webresource");
+                    query.ColumnSet.AddColumns("webresourceid", "displayname", "name", "content");
+                    query.Criteria.AddCondition("content", ConditionOperator.NotNull);
+                    query.Criteria.AddCondition("name", ConditionOperator.BeginsWith, "span_/js/");
+                    query.Criteria.AddCondition("webresourcetype", ConditionOperator.Equal, 3);
+
+                    args.Result = Service.RetrieveMultiple(query);
+                },
+                PostWorkCallBack = (args) =>
+                {
+                    if (args.Error != null)
+                    {
+                        MessageBox.Show(args.Error.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    var result = args.Result as EntityCollection;
+
+                    ListOfJS.Items.Clear();
+
+                    if (result != null)
+                    {
+
+                        var items = new List<ListObject>();
+                        foreach (var resource in result.Entities)
+                        {
+                            var content = (string)resource["content"];
+
+                            byte[] data = Convert.FromBase64String(content);
+                            string decodedString = System.Text.Encoding.UTF8.GetString(data);
+
+                            if (!decodedString.Contains(apiLogName)) continue;
+
+                            var name = (string)resource["name"];
+                            var id = (Guid)resource["webresourceid"];
+                            
+                            ListViewItem item = new ListViewItem(name);
+                            item.SubItems.Add(id.ToString());
+                            ListOfJS.Items.Add(item);
+
+                        }
+                    }
+                }
+
+            });
+        }
+        private void LoadBtn_Click(object sender, EventArgs e)
+        {
+            ExecuteMethod(GetAPIs);
+        }
+
+        private void OnAPISelected(object sender, EventArgs e)
+        {
+            ExecuteMethod(GetFlows);
+            ExecuteMethod(GetJS);
+        }
+
+        private void ListOfFlows_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InitializeFlowView()
+        {
+            ListOfFlows.View = View.Details;
+            ListOfFlows.FullRowSelect = true;
+            ListOfFlows.GridLines = true;
+
+            ListOfFlows.Columns.Add("Flow Name", 250);
+            ListOfFlows.Columns.Add("Link", 50);
+        }
+
+        private void InitializeJSView()
+        {
+            ListOfJS.View = View.Details;
+            ListOfJS.FullRowSelect = true;
+            ListOfJS.GridLines = true;
+
+            ListOfJS.Columns.Add("JS Name", 250);
+            ListOfJS.Columns.Add("Id", 50);
+        }
+
+        private void LoadButton_Click(object sender, EventArgs e)
+        {
+            ExecuteMethod(GetAPIs);
+        }
+
+        private void ShowCode()
+        {            
+            var selected = ListOfJS.SelectedItems[0] as ListViewItem;
+            if (selected == null) return;
+
+            var id = selected.SubItems[1].Text;
+
+            var api = ComboboxAPIs.SelectedItem as ListObject;
+            if (api == null) return;
+
+            var apiLogName = api.value;
+            if (apiLogName == null) return;
+
+            WorkAsync(new WorkAsyncInfo()
+            {
+                AsyncArgument = null,
+                Work = (worker, args) =>
+                {
+                    var query = new QueryExpression("webresource");
+                    query.ColumnSet.AddColumns("webresourceid", "displayname", "name", "content");
+                    
+                    query.Criteria.AddCondition("webresourceid", ConditionOperator.Equal, id);
+
+                    args.Result = Service.RetrieveMultiple(query).Entities.FirstOrDefault();
+                },
+                PostWorkCallBack = (args) =>
+                {
+                    if (args.Error != null)
+                    {
+                        MessageBox.Show(args.Error.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    var result = args.Result as Entity;
+
+                    CodeText.Text = "";
+
+                    if (result != null)
+                    {
+                        var content = (string)result["content"];
+
+                        byte[] data = Convert.FromBase64String(content);
+                        string decodedString = System.Text.Encoding.UTF8.GetString(data);
+
+                        CodeText.Text = decodedString;
+
+                        int index = CodeText.Text.IndexOf(apiLogName);
+                        if (index >= 0)
+                        {
+                            // Set the cursor position to the start of the word
+                            CodeText.SelectionStart = index;
+                            // Set the cursor length to the length of the word (select the word)
+                            CodeText.SelectionLength = apiLogName.Length;
+                            // Scroll to the cursor
+                            CodeText.ScrollToCaret();
+
+                            CodeText.SelectionColor = Color.Red;
+                        }
+                    }
+                }
+
+            });
+        }
+
+        private void OnJSSeleted2(object sender, EventArgs e)
+        {
+            //if (ListOfJS.SelectedItems.Count != 1) return;
+            //ExecuteMethod(ShowCode);
+        }
+
+        private void Solution_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnJSSelected(object sender, EventArgs e)
+        {
+            if (ListOfJS.SelectedItems.Count != 1) return;
+            ExecuteMethod(ShowCode);
+        }
+    }
+}
---
+++ MyPluginControl.designer.cs
@@ -0,0 +1,216 @@
+﻿using System;
+
+namespace MyTool
+{
+    partial class MyPluginControl
+    {
+        /// <summary> 
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary> 
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas 
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MyPluginControl));
+            this.ComboboxAPIs = new System.Windows.Forms.ComboBox();
+            this.ListOfFlows = new System.Windows.Forms.ListView();
+            this.toolStrip2 = new System.Windows.Forms.ToolStrip();
+            this.LoadButton = new System.Windows.Forms.ToolStripButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ListOfJS = new System.Windows.Forms.ListView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.Solution = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.CodeText = new System.Windows.Forms.RichTextBox();
+            this.toolStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ComboboxAPIs
+            // 
+            this.ComboboxAPIs.FormattingEnabled = true;
+            this.ComboboxAPIs.Location = new System.Drawing.Point(22, 127);
+            this.ComboboxAPIs.Name = "ComboboxAPIs";
+            this.ComboboxAPIs.Size = new System.Drawing.Size(291, 21);
+            this.ComboboxAPIs.TabIndex = 1;
+            this.ComboboxAPIs.SelectedIndexChanged += new System.EventHandler(this.OnAPISelected);
+            // 
+            // ListOfFlows
+            // 
+            this.ListOfFlows.HideSelection = false;
+            this.ListOfFlows.Location = new System.Drawing.Point(346, 72);
+            this.ListOfFlows.Name = "ListOfFlows";
+            this.ListOfFlows.Size = new System.Drawing.Size(328, 300);
+            this.ListOfFlows.TabIndex = 2;
+            this.ListOfFlows.UseCompatibleStateImageBehavior = false;
+            this.ListOfFlows.SelectedIndexChanged += new System.EventHandler(this.ListOfFlows_SelectedIndexChanged);
+            // 
+            // toolStrip2
+            // 
+            this.toolStrip2.ImageScalingSize = new System.Drawing.Size(28, 28);
+            this.toolStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.LoadButton});
+            this.toolStrip2.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip2.Name = "toolStrip2";
+            this.toolStrip2.Size = new System.Drawing.Size(1804, 35);
+            this.toolStrip2.TabIndex = 3;
+            this.toolStrip2.Text = "toolStrip2";
+            // 
+            // LoadButton
+            // 
+            this.LoadButton.Image = ((System.Drawing.Image)(resources.GetObject("LoadButton.Image")));
+            this.LoadButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.Size = new System.Drawing.Size(91, 32);
+            this.LoadButton.Text = "Load APIs";
+            this.LoadButton.Click += new System.EventHandler(this.LoadButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(343, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Flows:";
+            // 
+            // ListOfJS
+            // 
+            this.ListOfJS.HideSelection = false;
+            this.ListOfJS.Location = new System.Drawing.Point(689, 72);
+            this.ListOfJS.Name = "ListOfJS";
+            this.ListOfJS.Size = new System.Drawing.Size(320, 300);
+            this.ListOfJS.TabIndex = 6;
+            this.ListOfJS.UseCompatibleStateImageBehavior = false;
+            this.ListOfJS.SelectedIndexChanged += new System.EventHandler(this.OnJSSelected);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(686, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "JS:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(22, 72);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(291, 21);
+            this.comboBox2.TabIndex = 8;
+            // 
+            // Solution
+            // 
+            this.Solution.AutoSize = true;
+            this.Solution.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Solution.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Solution.Location = new System.Drawing.Point(19, 52);
+            this.Solution.Name = "Solution";
+            this.Solution.Size = new System.Drawing.Size(57, 13);
+            this.Solution.TabIndex = 9;
+            this.Solution.Text = "Solution:";
+            this.Solution.Click += new System.EventHandler(this.Solution_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(19, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "API:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(1024, 52);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Code:";
+            // 
+            // CodeText
+            // 
+            this.CodeText.Location = new System.Drawing.Point(1027, 72);
+            this.CodeText.Name = "CodeText";
+            this.CodeText.Size = new System.Drawing.Size(465, 481);
+            this.CodeText.TabIndex = 13;
+            this.CodeText.Text = "";
+            // 
+            // MyPluginControl
+            // 
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.Controls.Add(this.CodeText);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Solution);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ListOfJS);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.toolStrip2);
+            this.Controls.Add(this.ListOfFlows);
+            this.Controls.Add(this.ComboboxAPIs);
+            this.Name = "MyPluginControl";
+            this.Size = new System.Drawing.Size(1804, 958);
+            this.Load += new System.EventHandler(this.MyPluginControl_Load);
+            this.toolStrip2.ResumeLayout(false);
+            this.toolStrip2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ToolStrip toolStripMenu;
+        private System.Windows.Forms.ToolStripButton tsbClose;
+        private System.Windows.Forms.ToolStripButton tsbSample;
+        private System.Windows.Forms.ToolStripSeparator tssSeparator1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ListView flowsList;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox ComboboxAPIs;
+        private System.Windows.Forms.ListView ListOfFlows;
+        private System.Windows.Forms.ToolStrip toolStrip2;
+        private System.Windows.Forms.ToolStripButton LoadButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView ListOfJS;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label Solution;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox CodeText;
+    }
+}
